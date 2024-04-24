7d2bdd5fd2b56d0805cff7cbb3f13f41dbd40c11
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-04-23 21:17:16 -0500

    "Se crean los models"

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+namespace Gestion_de_turnos_la_misericordia.Models
+{
+    public class User
+    {
+    public int Id {get;set;}
+    public int DocumentType {get;set;}
+    public int DocumentNumber {get;set;}
+    public string Firstname {get;set;}
+    public string Lastname  {get;set;}
+    public string Email {get;set;}
+    public string Phone {get;set;}
+    }
+}
--- obj/Debug/net8.0/Gestion-de-turnos-la-misericordia.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gestion-de-turnos-la-misericordia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestion-de-turnos-la-misericordia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7273b8adace6d4632ec3c3d4bb184a9662603b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52e96706f4dbaec1412d3ed33eb126a70bc6d75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion-de-turnos-la-misericordia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion-de-turnos-la-misericordia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
