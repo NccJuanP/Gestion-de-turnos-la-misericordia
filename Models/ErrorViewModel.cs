d5a0a0c048b375833b5d8253f623b37677d35910
Juan Pablo <juanpablolc414@gmail.com>
2024-04-25 16:32:57 -0500

    arreglado

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,10 @@
-namespace Gestion_de_turnos_la_misericordia.Models;
+namespace Gestion_de_turnos_la_misericordia.Models
+{
 
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
 }
