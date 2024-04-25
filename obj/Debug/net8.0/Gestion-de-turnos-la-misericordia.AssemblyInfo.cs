40c3660a17a2f438d7a61643292c6fed269d41b0
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-04-24 20:50:32 -0500

    Se crea la session para que persistan los datos

--- obj/Debug/net8.0/Gestion-de-turnos-la-misericordia.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gestion-de-turnos-la-misericordia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestion-de-turnos-la-misericordia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1db7c4fc450f1397f210d5b7f87176faf0deee5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc94ca357cfed82d364168972b6bbdebc2419ede")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion-de-turnos-la-misericordia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion-de-turnos-la-misericordia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
