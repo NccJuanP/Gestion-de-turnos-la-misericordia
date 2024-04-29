a58a2b4b004420b89b48658b5aa4480ee4f6e1cd
AnthonyCarmine <anthonymunoz302755@correo.itm.edu.co>
2024-04-28 19:59:25 -0500

    Se crea la logica para que se suban los datos a la tabla Attention

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -20,14 +20,34 @@ namespace Misericordia.Controllers
             _context = context;
         }
 
-        public IActionResult PriorityUser(string priority)
+        int contadorUser = 0;
+
+public class UserAttentionViewModel
 {
-    if (!string.IsNullOrEmpty(priority))
+    public User User { get; set; }
+    public Attention Attention { get; set; }
+}
+    [HttpPost]
+        public async Task<IActionResult> PriorityUser(string priority)
+{
+   if (!string.IsNullOrEmpty(priority))
+{
+    HttpContext.Session.SetString("priority", priority);
+
+    int priorityOfUser = Convert.ToInt32(priority);
+
+    var attention = new Attention
     {
-        HttpContext.Session.SetString("priority", priority);
-        return RedirectToAction("UserNew");
-    }
-    return View();
+        AttentionPreference = priorityOfUser
+        
+    };
+
+    _context.Attentions.Add(attention);
+    await _context.SaveChangesAsync();
+
+    return RedirectToAction("UserNew");
+}
+return View();
 }
 
         public IActionResult UserNew(string userType)
@@ -98,8 +118,8 @@ public IActionResult TypeRequestUser(string typeRequest)
     
     {
         HttpContext.Session.SetString("typeRequest", typeRequest);
-      
-             return RedirectToAction("Ficho");
+            TempData["typeRequest"] = typeRequest;
+            return RedirectToAction("Ficho");
     }
     return View();
 }
@@ -129,36 +149,71 @@ public IActionResult Gestion()
     return View(turns);
 }
 
+[HttpPost]
 public async Task<IActionResult> Ficho(int? id)
 {
-      string typeOfRequest = HttpContext.Session.GetString("typeRequest");
-    if (typeOfRequest == "1"){
+    string typeOfRequest = TempData["typeRequest"] as string;
+    if (typeOfRequest == "1")
+    {
         ViewBag.typeOfRequest = "GC";
-    }else if (typeOfRequest == "2"){
+    }
+    else if (typeOfRequest == "2")
+    {
         ViewBag.typeOfRequest = "IF";
-    }else if (typeOfRequest == "3"){
+    }
+    else if (typeOfRequest == "3")
+    {
         ViewBag.typeOfRequest = "PF";
-    }else if (typeOfRequest == "4"){
+    }
+    else if (typeOfRequest == "4")
+    {
         ViewBag.typeOfRequest = "AM";
     }
 
     string typeOfDocument = HttpContext.Session.GetString("typeDocument");
 
-    if (typeOfDocument == "1"){
+    if (typeOfDocument == "1")
+    {
         ViewBag.typeOfDocument = "CC";
-    }else if (typeOfDocument == "2"){
+    }
+    else if (typeOfDocument == "2")
+    {
         ViewBag.typeOfDocument = "TI";
-    }else if (typeOfDocument == "3"){
+    }
+    else if (typeOfDocument == "3")
+    {
         ViewBag.typeOfDocument = "CE";
-    }else if (typeOfDocument == "4"){
+    }
+    else if (typeOfDocument == "4")
+    {
         ViewBag.typeOfDocument = "DA";
-    }    
-        
-          var userJson = HttpContext.Session.GetString("user");
+    }
+
+    var userJson = HttpContext.Session.GetString("user");
     var user = JsonConvert.DeserializeObject<User>(userJson);
 
-    return View(user);
-}
+    contadorUser++;
+    var newTurn = contadorUser.ToString() + "-" + ViewBag.typeOfDocument;
 
-    }
+    var attention = new Attention
+    {
+        NumAttention = newTurn,
+        UserId = user.Id,
+        DateAttentionEnter = DateTime.Now
+    };
+
+    _context.Attentions.Update(attention);
+    await _context.SaveChangesAsync();
+
+     var viewModel = new UserAttentionViewModel
+    {
+        User = user,
+        Attention = attention
+    };
+
+    return View(viewModel);
 }
+}
+  } 
+
+
